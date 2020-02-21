ea9c6def131df62453e10c175345bcf872603bcd
Timofei Tverdokhlebov <great.timofey@icloud.com>
2020-02-21 08:56:33 +0700

    add eslint, prettier, lint-staged

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,46 +1,11 @@
 import React from 'react'
-import {View, StyleSheet, Text} from 'react-native'
-
+import { View, StyleSheet, Text } from 'react-native'
 import {
-  Header,
   LearnMoreLinks,
   Colors,
   DebugInstructions,
   ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Body = () => {
-  return (
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-  )
-}
+} from 'react-native/Libraries/NewAppScreen'
 
 const styles = StyleSheet.create({
   body: {
@@ -64,6 +29,37 @@ const styles = StyleSheet.create({
   highlight: {
     fontWeight: '700',
   },
-            })
+})
 
-export default Body;
+const Body = () => {
+  return (
+    <View style={styles.body}>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Step ne</Text>
+        <Text style={styles.sectionDescription}>
+          Edit <Text style={styles.highlight}>App.js</Text> to change this screen and then come back
+          to see your edits.
+        </Text>
+      </View>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>See Your Changes</Text>
+        <Text style={styles.sectionDescription}>
+          <ReloadInstructions />
+        </Text>
+      </View>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Debug</Text>
+        <Text style={styles.sectionDescription}>
+          <DebugInstructions />
+        </Text>
+      </View>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Learn More</Text>
+        <Text style={styles.sectionDescription}>Read the docs to discover what to do next:</Text>
+      </View>
+      <LearnMoreLinks />
+    </View>
+  )
+}
+
+export default Body
